c5beb81c38941f092c5e6c2601a25bbb5141dec4
Alexey Gaziev <alex.gaziev@gmail.com>
2015-08-14 15:22:18 +0700

    Plugin class

--- lib/battletoads/plugin.rb
+++ lib/battletoads/plugin.rb
@@ -1,4 +1,18 @@
-class Battletoads
+module Battletoads
   class Plugin
+
+    attr_accessor :name, :postcss_arguments, :styles, :examples
+
+    def initialize(opts)
+      @name              = opts['name']
+      @postcss_arguments = opts['postcss_arguments']
+      @styles            = opts['styles'] || []
+      @examples          = opts['examples'] || []
+    end
+
+    def escaped_name
+      "\"#{name}\""
+    end
+
   end
 end
